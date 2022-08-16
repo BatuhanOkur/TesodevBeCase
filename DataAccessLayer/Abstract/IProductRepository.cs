acf1bdc08da88558dab875186d45de243a1592a4
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 15:04:16 +0300

    ProductRepository interface has been created.

--- DataAccessLayer/Abstract/IProductRepository.cs
+++ DataAccessLayer/Abstract/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EntityLayer.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
+        List<Product> GetProducts();
+
+        Product GetProduct(Guid id);
+
+        Product CreateProduct(Product product);
+
+        Product UpdateProduct(Product product);
+
+        void DeleteProduct(Guid id);
     }
 }
