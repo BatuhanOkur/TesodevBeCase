06a63b282dc09e010544a46af80f1fa2259e38dd
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 14:17:43 +0300

    Layer has been created

---
+++ EntityLayer/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int CustomerId { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public int Status { get; set; }
+        public Product Product { get; set; }
+        public Address Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
