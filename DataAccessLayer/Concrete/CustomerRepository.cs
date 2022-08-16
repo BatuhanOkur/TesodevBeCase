75c9bb1e1a070f9e24430194419bf05feca802d9
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 15:24:49 +0300

    CustomerRepository has been created

---
+++ DataAccessLayer/Concrete/CustomerRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        Context dbContext = new Context();
+        public Customer CreateCustomer(Customer customer)
+        {
+            dbContext.Customers.Add(customer);
+            dbContext.SaveChanges();
+            return customer;
+        }
+
+        public void DeleteCustomer(Guid id)
+        {
+            var deletedCustomer = GetCustomer(id);
+            dbContext.Customers.Remove(deletedCustomer);
+            dbContext.SaveChanges();
+        }
+
+        public Customer GetCustomer(Guid id)
+        {
+            return dbContext.Customers.Find(id);
+        }
+
+        public List<Customer> GetCustormers()
+        {
+            return dbContext.Customers.ToList();
+        }
+
+        public Customer UpdateCustomer(Customer customer)
+        {
+            dbContext.Customers.Update(customer);
+            dbContext.SaveChanges();
+            return customer;
+        }
+    }
+}
