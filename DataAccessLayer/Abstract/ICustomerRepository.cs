5db23db74d765c994b37cda25559e69076c7ffbe
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 14:58:51 +0300

    Abstract folder has been created in DataAccesLayer.

---
+++ DataAccessLayer/Abstract/ICustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICustomerRepository
+    {
+        List<Customer> GetCustormers();
+
+        Customer GetCustomer(Guid id);
+
+        Customer CreateCustomer(Customer customer);
+
+        Customer UpdateCustomer(Customer customer);
+
+        void DeleteCustomer(Guid id);
+    }
+}
