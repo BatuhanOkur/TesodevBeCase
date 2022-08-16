850876fecc213be3800af3748419186755406411
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 15:13:28 +0300

    AddressRepository has been created in Concrete

---
+++ DataAccessLayer/Concrete/AddressRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class AddressRepository : IAddressRepository
+    {
+        Context dbContext = new Context();
+        public Address CreateAddress(Address address)
+        {
+            dbContext.Addresses.Add(address);
+            dbContext.SaveChanges();
+            return address;
+        }
+
+        public void DeleteAddress(Guid id)
+        {
+            var deletedAdress = GetAddress(id);
+            dbContext.Addresses.Remove(deletedAdress);
+            dbContext.SaveChanges();
+        }
+
+        public Address GetAddress(Guid id)
+        {
+            return dbContext.Addresses.Find(id);
+        }
+
+        public List<Address> GetAddresses()
+        {
+            return dbContext.Addresses.ToList();
+        }
+
+        public Address UpdateAddress(Address address)
+        {
+            dbContext.Addresses.Update(address);
+            dbContext.SaveChanges();
+            return address;
+        }
+    }
+}
