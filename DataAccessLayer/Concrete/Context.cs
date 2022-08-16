f9f99d6c9a31e33e47880639665da6dc1ddec87b
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 14:59:39 +0300

    Concrete folder has been created in DataAccesLayer

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-35OUD05\\SQLEXPRESS;database=TesodevDb; integrated security=true;");
+        }
+
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
