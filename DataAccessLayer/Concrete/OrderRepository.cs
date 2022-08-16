3383eb63a442f12154caa24697df573b746b930f
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-16 15:25:04 +0300

    OrderRepository has been created

---
+++ DataAccessLayer/Concrete/OrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class OrderRepository : IOrderRepository
+    {
+        Context dbContext = new Context();
+        public Order CreateOrder(Order order)
+        {
+            dbContext.Orders.Add(order);
+            dbContext.SaveChanges();
+            return order;
+        }
+
+        public void DeleteOrder(Guid id)
+        {
+            var deletedOrder = GetOrder(id);
+            dbContext.Orders.Remove(deletedOrder);
+            dbContext.SaveChanges();
+        }
+
+        public Order GetOrder(Guid id)
+        {
+            return dbContext.Orders.Find(id);
+        }
+
+        public List<Order> GetOrders()
+        {
+            return dbContext.Orders.ToList();
+        }
+
+        public Order UpdateOrder(Order order)
+        {
+            dbContext.Orders.Update(order);
+            dbContext.SaveChanges();
+            return order;
+        }
+    }
+}
