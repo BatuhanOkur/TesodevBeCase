a9c589321e89f4160e8aec507f5055228c8f471a
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-17 18:17:40 +0300

    Req fields have been added in Context.cs

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -21,5 +21,46 @@ namespace DataAccessLayer.Concrete
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Primary Keys
+            modelBuilder.Entity<Customer>().HasKey(s => s.Id);
+            modelBuilder.Entity<Order>().HasKey(s => s.Id);
+            modelBuilder.Entity<Product>().HasKey(s => s.Id);
+            modelBuilder.Entity<Address>().HasKey(s => s.CityCode);
+
+            //Customer Properties
+            modelBuilder.Entity<Customer>().Property(s => s.Name).IsRequired();
+            modelBuilder.Entity<Customer>().Property(s => s.Email).IsRequired();
+            modelBuilder.Entity<Customer>().Property(s => s.UpdatedAt).IsRequired();
+            modelBuilder.Entity<Customer>().Property(s => s.CreatedAt).IsRequired();
+
+            modelBuilder.Entity<Customer>().HasOne(s => s.Address).WithMany().IsRequired();
+
+            //Order Properties
+            modelBuilder.Entity<Order>().Property(s => s.Quantity).IsRequired();
+            modelBuilder.Entity<Order>().Property(s => s.Price).IsRequired();
+            modelBuilder.Entity<Order>().Property(s => s.Status).IsRequired();
+            modelBuilder.Entity<Order>().Property(s => s.Quantity).IsRequired();
+            modelBuilder.Entity<Order>().Property(s => s.CreatedAt).IsRequired();
+            modelBuilder.Entity<Order>().Property(s => s.UpdatedAt).IsRequired();
+
+            modelBuilder.Entity<Order>().HasOne(s => s.Address).WithMany().IsRequired();
+            modelBuilder.Entity<Order>().HasOne(s => s.Product).WithMany().IsRequired();
+
+            //Address Properties
+            modelBuilder.Entity<Address>().Property(s => s.AddressLine).IsRequired();
+            modelBuilder.Entity<Address>().Property(s => s.City).IsRequired();
+            modelBuilder.Entity<Address>().Property(s => s.CityCode).IsRequired();
+            modelBuilder.Entity<Address>().Property(s => s.Country).IsRequired();
+            //Product Properties
+            modelBuilder.Entity<Product>().Property(s => s.ImageUrl).IsRequired();
+            modelBuilder.Entity<Product>().Property(s => s.Name).IsRequired();
+
+            //Connections
+
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
