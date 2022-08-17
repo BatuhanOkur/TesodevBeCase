3707cd9c6b84fb7f1bc5715c6a15db838b93f314
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-17 15:10:51 +0300

    CustomerController has been created

---
+++ WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("webapi/[controller]s")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerManager customerManager = new CustomerManager();
+
+        [HttpPost]
+        public Customer Create([FromBody] Customer customer)
+        {
+            return customerManager.CreateCustomer(customer);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Customer> GetAll()
+        {
+            return customerManager.GetCustomers();
+        }
+
+        [HttpGet("{id}")]
+        public Customer Get(Guid id)
+        {
+            return customerManager.GetCustomer(id);
+        }
+
+        [HttpPut]
+        public Customer Update([FromBody] Customer customer)
+        {
+            return customerManager.UpdateCustomer(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            customerManager.DeleteCustomer(id);
+        }
+    }
+}
