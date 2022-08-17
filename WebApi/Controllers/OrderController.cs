fcac4eb334171a8c6c89864fb2467301a1d49bf1
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-17 15:11:14 +0300

    OrderController has been created

---
+++ WebApi/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("webapi/[controller]s")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        OrderManager orderManager = new OrderManager();
+
+        [HttpPost]
+        public Order Create([FromBody] Order order)
+        {
+            return orderManager.CreateOrder(order);
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public List<Order> GetAll()
+        {
+            return orderManager.GetOrders();
+        }
+
+        [HttpGet("{id}")]
+        public Order Get(Guid id)
+        {
+            return orderManager.GetOrder(id);
+        }
+
+        [HttpPut]
+        public Order Update([FromBody] Order order)
+        {
+            return orderManager.UpdateOrder(order);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+            orderManager.DeleteOrder(id);
+        }
+    }
+}
