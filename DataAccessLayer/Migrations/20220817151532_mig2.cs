75c6b814e1b839226257da6a904d09d82276d346
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-17 18:18:49 +0300

    Mig2 done

---
+++ DataAccessLayer/Migrations/20220817151532_mig2.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Addresses_AddressLine",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_AddressLine",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Products_ProductId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AddressLine",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_AddressLine",
+                table: "Customers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AddressLine",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressCityCode",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AddressCityCode",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityCode",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressLine",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "CityCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AddressCityCode",
+                table: "Orders",
+                column: "AddressCityCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_AddressCityCode",
+                table: "Customers",
+                column: "AddressCityCode");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Addresses_AddressCityCode",
+                table: "Customers",
+                column: "AddressCityCode",
+                principalTable: "Addresses",
+                principalColumn: "CityCode",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_AddressCityCode",
+                table: "Orders",
+                column: "AddressCityCode",
+                principalTable: "Addresses",
+                principalColumn: "CityCode",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Products_ProductId",
+                table: "Orders",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Addresses_AddressCityCode",
+                table: "Customers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_AddressCityCode",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Products_ProductId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_AddressCityCode",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_AddressCityCode",
+                table: "Customers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "AddressCityCode",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "AddressCityCode",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "Orders",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressLine",
+                table: "Customers",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AddressLine",
+                table: "Addresses",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityCode",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "AddressLine");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_AddressLine",
+                table: "Orders",
+                column: "AddressLine");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_AddressLine",
+                table: "Customers",
+                column: "AddressLine");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Addresses_AddressLine",
+                table: "Customers",
+                column: "AddressLine",
+                principalTable: "Addresses",
+                principalColumn: "AddressLine",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_AddressLine",
+                table: "Orders",
+                column: "AddressLine",
+                principalTable: "Addresses",
+                principalColumn: "AddressLine",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Products_ProductId",
+                table: "Orders",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
