1778accdba9609df6be03f92e7b66debba030226
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-18 16:06:15 +0300

    ValidationRules has been specified.

---
+++ BusinessLayer/ValidationRules/CustomerValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Customer name can't be empty.");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("Customer name must be over 3 characters.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email is not a valid email address.");
+        }
+    }
+}
---
+++ BusinessLayer/ValidationRules/OrderValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class OrderValidator : AbstractValidator<Order>
+    {
+        public OrderValidator()
+        {
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price can't be empty.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+            RuleFor(x => x.Quantity).NotEmpty().WithMessage("Quantity can't be empty.");
+            RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0.");
+            RuleFor(x => x.CustomerId).NotEmpty().WithMessage("Customer ID can't be empty.");
+            RuleFor(x => x.Address).NotEmpty().WithMessage("Address can't be empty.");
+        }
+    }
+}
