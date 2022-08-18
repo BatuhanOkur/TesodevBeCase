133156b8edd1ce92b92efd40c1da26415faa8644
Batuhan Okur <batuhanokur2209@gmail.com>
2022-08-18 14:49:42 +0300

    CustomerController has been updated.

--- WebApi/Controllers/CustomerController.cs
+++ WebApi/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.Abstract;
 using EntityLayer.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,41 +10,46 @@ using System.Threading.Tasks;
 
 namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("webapi/[controller]s")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        CustomerManager customerManager = new CustomerManager();
+        ICustomerService _customerService;
+
+        public CustomerController()
+        {
+            _customerService = new CustomerManager();
+        }
 
         [HttpPost]
         public Customer Create([FromBody] Customer customer)
         {
-            return customerManager.CreateCustomer(customer);
+            return _customerService.CreateCustomer(customer);
         }
 
         [HttpGet]
         [Route("GetAll")]
         public List<Customer> GetAll()
         {
-            return customerManager.GetCustomers();
+            return _customerService.GetCustomers();
         }
 
         [HttpGet("{id}")]
         public Customer Get(Guid id)
         {
-            return customerManager.GetCustomer(id);
+            return _customerService.GetCustomer(id);
         }
 
         [HttpPut]
         public Customer Update([FromBody] Customer customer)
         {
-            return customerManager.UpdateCustomer(customer);
+            return _customerService.UpdateCustomer(customer);
         }
 
         [HttpDelete("{id}")]
         public void Delete(Guid id)
         {
-            customerManager.DeleteCustomer(id);
+            _customerService.DeleteCustomer(id);
         }
     }
 }
